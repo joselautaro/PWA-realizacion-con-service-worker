e88bd215b93338c9d4b180aae9dd527d1d467ff7
Lautaro Montaña <joselautarom@gmail.com>
2022-08-22 21:59:44 -0300

    Creamos service worker y vamos escribiendo paso a paso clases.txt

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('fetch' , event => {
+    
+    if(event.request.url.includes('style.css')) {
+        event.respondWith(null);
+    } else{
+        event.respondWith(fetch(event.request));
+    }
+
+
+})
