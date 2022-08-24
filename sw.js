1dc644b003d3ce3897412644a3b1aa384c84fcd0
Lautaro Montaña <joselautarom@gmail.com>
2022-08-24 19:47:04 -0300

    Como interceptar una petición fetch y cambiar de estilos nuestro html

--- sw.js
+++ sw.js
@@ -1,14 +1,27 @@
 self.addEventListener('fetch' , event => {
     
-    if(event.request.url.includes('.jpg')) {
-        console.log(event.request.url)
+    if(event.request.url.includes('style.css')) {
+        // console.log(event.request.url)
 
         // let fotoReq = fetch('img/main.jpg)
         // let  fotoReq = fetch(event.request.url)
 
-        let fotoReq = (event.request);
+        // let fotoReq = (event.request);
 
-        event.respondWith(fotoReq);
+        // event.respondWith(fotoReq);
+
+            let respuesta = new Response (`
+                body{
+                    background-color: red; !important;
+                    color: white;
+                }
+            `,{
+                headers:{
+                'Content-Type': 'text/css'
+            }
+        } );
+        event.respondWith(respuesta);
+
     } 
 
 
